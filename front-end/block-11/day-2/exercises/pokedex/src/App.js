df0e1eeccdcb73915019434ef57783a800b5b4a7
Carlos Barros <carlos_dbs@hotmail.com>
2021-10-01 17:26:40 -0300

    Adiciona exercicio pokedex

---
+++ front-end/block-11/day-2/exercises/pokedex/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Pokedex from './components/Pokedex';
+
+function App() {
+  return (
+    <main className="App">
+      <Pokedex />
+    </main>
+  );
+}
+
+export default App;
