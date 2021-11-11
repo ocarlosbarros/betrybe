303cd7bf6664649f0e08c16a51ab85a8022678e3
Carlos Barros <carlos_dbs@hotmail.com>
2021-11-11 19:00:05 -0300

    Adicionando action types para aplicacao

--- front-end/block-15/day-1/exercises/exercise-1/src/js/index.js
+++ front-end/block-15/day-1/exercises/exercise-1/src/js/index.js
@@ -1,8 +1,16 @@
+
+
 const INITIAL_STATE = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
 
+//Definindo action types
+const NEXT_COLOR = 'NEXT_COLOR';
+const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
+
+
+//Criação do reducer
 const reducer =( state = INITIAL_STATE ) => {
   switch('action'){
     default:
@@ -10,4 +18,5 @@ const reducer =( state = INITIAL_STATE ) => {
   }
 }
 
+//Criação da store recebendo como parâmetro obrigatório o reducer para seu funcionamento
 const store = Redux.createStore(reducer);
