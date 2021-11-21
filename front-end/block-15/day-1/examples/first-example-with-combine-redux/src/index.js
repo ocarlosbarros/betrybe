571fbbc069330980de4bc9fb1e7808336ca2232e
Carlos Barros <carlos_dbs@hotmail.com>
2021-11-20 21:45:58 -0300

    Adiciona um subscribe na store

--- front-end/block-15/day-1/examples/first-example-with-combine-redux/src/index.js
+++ front-end/block-15/day-1/examples/first-example-with-combine-redux/src/index.js
@@ -2,8 +2,7 @@ import { store } from './store/index.js'
 import { createNameAction } from './actions/createName.js'
 import { createEmailAction } from './actions/createEmail.js'
 
-console.log(store.getState());
+
 store.dispatch(createNameAction('Bruce Wayne'));
-console.log(store.getState());
 store.dispatch(createEmailAction('bruce@wayne.com'));
-console.log(store.getState());
+
--- front-end/block-15/day-1/examples/first-example-with-combine-redux/src/store/index.js
+++ front-end/block-15/day-1/examples/first-example-with-combine-redux/src/store/index.js
@@ -3,4 +3,9 @@ import rootReducers from '../reducers/rootReducer.js';
 
 const store = createStore(rootReducers);
 
+//Adiciona um listener que executará uma callback toda vez que uma action é disparada
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
 export { store };
