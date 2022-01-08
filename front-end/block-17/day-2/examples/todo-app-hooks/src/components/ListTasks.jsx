70fb1cf19341aefe1318c78b51195b7d48b7646e
Carlos Barros <carlos_dbs@hotmail.com>
2022-01-08 16:59:15 -0300

    Refatorado componente de classe ListTasks para componenete funcional

--- front-end/block-17/day-2/examples/todo-app-hooks/src/components/ListTasks.jsx
+++ front-end/block-17/day-2/examples/todo-app-hooks/src/components/ListTasks.jsx
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
-
-class ListTasks extends Component {
-  render(){
-    const { tasks } = this.props;
-    return(
-      <section class="task-list">
-        <h2>Tarefas:</h2>
-        <ul>
-          { tasks.map((task, index) => (
-            <li key={`${index}-${task}`}>{ task }</li>
-          ))}
-        </ul>
-      </section>
-    );
-  }
+function ListTasks({ tasks }){
+  return(
+    <section class="task-list">
+      <h2>Tarefas:</h2>
+      <ul>
+        { tasks.map((task, index) => (
+          <li key={`${index}-${task}`}>{ task }</li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default ListTasks;
