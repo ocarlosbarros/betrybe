ca78bc1d9c9b40aefd5ed231023ed42047706a4f
Carlos Barros <carlos_dbs@hotmail.com>
2022-02-12 19:36:38 -0300

    Adiciona exercicios com todos os 'ingredientes' aprendidos

--- back-end/block-20/day-2/practices/DML_select-with-Limit-Offset-practice.sql
+++ back-end/block-20/day-2/practices/DML_select-with-Limit-Offset-practice.sql
@@ -4,4 +4,4 @@ SELECT * FROM sakila.rental LIMIT 10;
 
 SELECT * FROM sakila.rental LIMIT 10 OFFSET 3;
 
-SELECT * FROM sakila.actor LIMIT 10 OFFSET 4;
+SELECT * FROM sakila.actor LIMIT 10 OFFSET 5;
