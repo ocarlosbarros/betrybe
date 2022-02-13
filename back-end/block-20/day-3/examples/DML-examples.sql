47600b0558eec073e45f8d094a8b1766d523a739
Carlos Barros <carlos_dbs@hotmail.com>
2022-02-12 22:21:16 -0300

    Adiciona exercicios para praticar

--- back-end/block-20/day-3/examples/DML-examples.sql
+++ back-end/block-20/day-3/examples/DML-examples.sql
@@ -1,39 +1,52 @@
+USE sakila;
+
 SELECT * FROM actor;
 
-SELECT * FROM actor WHERE actor_id = 101;
+SELECT * FROM actor
+	WHERE last_name = 'Davis';
+
+SELECT * FROM actor
+	WHERE actor_id = 101;
+    
 
 SELECT * FROM film;
 
-SELECT * FROM film WHERE length > 50;
+SELECT * FROM film 
+	WHERE length > 50
+		ORDER BY length;
+        
+SELECT * FROM film 
+	WHERE length < 50
+		ORDER BY length;
+        
+SELECT * FROM film 
+	WHERE title <> 'ALIEN CENTER'
+		ORDER BY length;
+        
+SELECT * FROM film 
+	WHERE title <> 'ALIEN CENTER' AND replacement_cost > 20;
+		
 
-SELECT * FROM film WHERE length > 50 ORDER BY title;
+SELECT * FROM film 
+	WHERE rating = 'G' OR rating = 'PG' OR rating = 'PG-13'; 		
+    
 
-SELECT COUNT(*) FROM film WHERE length > 50;
+SELECT * FROM rental
+	WHERE return_date IS NULL;
+    
 
-SELECT * FROM film WHERE length < 50 ORDER BY length;
+SELECT * FROM staff
+	WHERE `active` IS TRUE;
 
-SELECT * FROM film WHERE title <> 'ALIEN CENTER';
+SELECT * FROM staff
+	WHERE `active` IS NOT TRUE;
+    
+SELECT * FROM address
+	WHERE address2 IS NOT NULL;
+    
+SELECT * FROM address
+	WHERE address2 = '';
+    
+SELECT * FROM film
+	WHERE title NOT LIKE 'academy%';
 
-SELECT * FROM film WHERE title <> 'ALIEN CENTER' AND replacement_cost > 20;
-
-SELECT * FROM film WHERE title <> 'ALIEN CENTER' AND replacement_cost > 20;
-
-SELECT * FROM film WHERE rating = 'G' OR rating = 'PG' OR rating = 'PG-13';
-
-SELECT * FROM rental WHERE return_date IS NULL;
-
-SELECT * FROM staff WHERE active IS TRUE;
-
-SELECT * FROM staff WHERE active IS FALSE;
-
-SELECT * FROM staff WHERE active IS NULL;
-
-SELECT * FROM staff WHERE active IS NOT TRUE;
-
-SELECT * FROM address;
-
-SELECT * FROM address WHERE address2 IS NOT NULL;
-
-SELECT * FROM address WHERE address2 IS NULL;
-
-SELECT * FROM film WHERE title NOT LIKE 'academy%';
