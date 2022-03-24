e471ec14416abab5600241fcecc7cd92aff12b33
Carlos Barros <carlos_dbs@hotmail.com>
2022-03-23 21:28:56 -0300

    feat:adiciona exercicios bonus do banco de dados da pixar

---
+++ back-end/block-20/day-4/exercises/exercise-pixar-bonus.sql
@@ -0,0 +1,24 @@
+-- Exercício 8 : Altere a classificação da tabela BoxOffice para 9.0 
+-- de todos os filmes que lucraram mais de 400 milhões no mercado interno.
+    
+UPDATE Pixar.BoxOffice
+	SET rating = 9.0
+	WHERE domestic_sales > 400000000;
+
+
+-- Exercício 9 : Altere a classificação da tabela BoxOffice para 6.0
+-- de todos os filmes que lucraram menos de 300 milhões no mercado 
+-- internacional e mais de 200 milhões no mercado interno.
+
+UPDATE Pixar.BoxOffice
+	SET rating = 6.0
+	WHERE domestic_sales > 200000000 AND international_sales < 300000000;
+
+
+-- Exercício 10 : Exclua da tabela Movies todos os filmes com menos de 100 minutos de duração.
+
+DELETE FROM Pixar.BoxOffice
+	WHERE movie_id IN (1, 6, 7, 8);
+
+DELETE FROM Pixar.Movies
+    WHERE length_minutes < 100;
